06769667c9e650f6a09059b8845c37018a3d9cea
AaradhyaGitHub <aaradhyagithub@gmail.com>
2025-12-22 19:51:00 -0800

    Added template for tail delete

---
+++ LinkedLists/Delete/tail_delete_LL.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+#include "Node.h"
+using namespace std;
+
+void tail_delete(Node **head)
+{
+    Node *current = *head;
+
+    while (current != nullptr)
+    {
+        cout << current->data << endl;
+        if (current->next == nullptr)
+        {
+            cout << "City to delete: " << current->data << endl;
+        }
+
+        current = current->next;
+    }
+}
+
+int main()
+{
+    Node *el_cerrito = new Node("El Cerrito");
+    Node *albany = new Node("Albany");
+    Node *berkeley = new Node("Berkeley");
+    Node *emeryville = new Node("Emeryville");
+    Node *oakland = new Node("Oakland");
+    Node *san_francisco = new Node("San Francisco");
+
+    el_cerrito->prev = nullptr;
+    el_cerrito->next = albany;
+
+    albany->prev = el_cerrito;
+    albany->next = berkeley;
+
+    berkeley->prev = albany;
+    berkeley->next = emeryville;
+
+    emeryville->prev = berkeley;
+    emeryville->next = oakland;
+
+    oakland->prev = emeryville;
+    oakland->next = san_francisco;
+
+    san_francisco->prev = oakland;
+    san_francisco->next = nullptr;
+
+    Node *head = el_cerrito;
+
+    cout << "Initial list:" << endl;
+    Node *current = head;
+    while (current != nullptr)
+    {
+        cout << current->data << " -> ";
+        current = current->next;
+    }
+    cout << "END" << endl
+         << endl;
+
+    tail_delete(&head);
+
+    cout << "After deleting tail:" << endl;
+    current = head;
+    while (current != nullptr)
+    {
+        cout << current->data << " -> ";
+        current = current->next;
+    }
+    cout << "END" << endl;
+
+    // Clean up memory
+    current = head;
+    while (current != nullptr)
+    {
+        Node *temp = current;
+        current = current->next;
+        delete temp;
+    }
+
+    return 0;
+}
