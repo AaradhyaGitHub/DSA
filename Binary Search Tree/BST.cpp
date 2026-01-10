0849ff85ec919886973f847066346dc91a4fa3f4
AaradhyaGitHub <aaradhyagithub@gmail.com>
2026-01-09 22:02:44 -0800

    Finished bst insert

--- Binary Search Tree/BST.cpp
+++ Binary Search Tree/BST.cpp
@@ -20,16 +20,50 @@ class BST
 private:
     Node *root;
 
+    Node *insertHelper(Node *node, int val){
+        if(node == nullptr){
+            return new Node(val);
+        }
+
+        if (val < node->data){
+            node->left = insertHelper(node->left, val);
+        }
+        else if(val > node->data){
+            node->right = insertHelper(node->right, val);
+        }
+
+        return node;
+    }
+
 public:
     BST()
     {
         root = nullptr;
     }
+    void insert(int val)
+    {
+        if (root == nullptr)
+        {
+            root = new Node(val);
+            return;
+        }
 
-    void search(int val);
-    void insert(int val);
+        else
+        {
+            if (val < root->data)
+            {
+                if (root->left == nullptr)
+                {
+                    root->left = new Node(val);
+                }
+                else
+                {
+                    insert(root->left->data);
+                }
+            }
+        }
+    };
     ~BST();
-
 };
 
 int main()
