eaea702bf3f0693eb8f3688019d4ab2afe2156a0
AaradhyaGitHub <aaradhyagithub@gmail.com>
2026-01-14 12:00:44 -0800

    Cleanup of executable files and added merge sort template

---
+++ Sorting/merge_sort.cpp
@@ -0,0 +1,43 @@
+#include <vector>
+using namespace std;
+
+void merge(vector<int> &arr, int left, int mid, int right)
+{
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
+
+    vector<int> L(n1), R(n2);
+
+    // Copy data to temp arrays
+    for (int i = 0; i < n1; i++)
+        L[i] = arr[left + i];
+    for (int j = 0; j < n2; j++)
+        R[j] = arr[mid + 1 + j];
+
+    int i = 0, j = 0, k = left;
+
+    // Merge back into original array
+    while (i < n1 && j < n2)
+    {
+        if (L[i] <= R[j])
+        {
+            arr[k++] = L[i++];
+        }
+        else
+        {
+            arr[k++] = R[j++];
+        }
+    }
+
+    // Copy remaining elements of L (if any)
+    while (i < n1)
+    {
+        arr[k++] = L[i++];
+    }
+
+    // Copy remaining elements of R (if any)
+    while (j < n2)
+    {
+        arr[k++] = R[j++];
+    }
+}
