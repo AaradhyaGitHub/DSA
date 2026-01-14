955cccdbace6d53f27d26bc788069bc5d7216225
AaradhyaGitHub <aaradhyagithub@gmail.com>
2026-01-14 10:31:14 -0800

    Implemented Insertion sort

--- Sorting/insert.cpp
+++ Sorting/insert.cpp
@@ -6,6 +6,7 @@ void insertionSort(int arr[], int n)
     for (int i = 1; i < n; i++)
     {
         int current = arr[i];
+
         int j = i - 1;
 
         while (j >= 0 && arr[j] > current)
@@ -13,6 +14,7 @@ void insertionSort(int arr[], int n)
             arr[j + 1] = arr[j];
             j--;
         }
+
         arr[j + 1] = current;
     }
 }
